88c129fb987518e5d08f5425adb6be4c2c545b42
Fernando Puente-Sánchez <fpusan@gmail.com>
2019-03-27 11:13:46 +0100

    switch to TPM

--- application/libraries/ContigFileParser2.php
+++ application/libraries/ContigFileParser2.php
@@ -36,7 +36,7 @@ class ContigFileParser2 extends GameParser {
                     $this->col_names_idx[$header[$i]] = $i;
                     break;
                 default:
-                    if (strpos($header[$i], "RPKM") === 0) {
+                    if (strpos($header[$i], "TPM") === 0) {
                         $aux_sample = substr($header[$i], 5);
                         if (!isset($sample[$aux_sample]) || $sample[$aux_sample] == NULL) {
                             $this->error_message = "Wrong header: Unknown sample name '{$aux_sample}' in column {$i}";
